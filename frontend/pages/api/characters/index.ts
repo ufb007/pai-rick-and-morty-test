8cee9b034b85d4bc244e46bd7065c48db3450a8f
Ufuk Bozdemir <ufb007@gmail.com>
2023-11-28 22:22:53 +0000

    API request characters from file and returning

--- frontend/pages/api/characters/index.ts
+++ frontend/pages/api/characters/index.ts
@@ -1,13 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { promises as fs } from 'fs';
 
 /**
  * For more info on API routes please read the nextJS docs here
  * https://nextjs.org/docs/api-routes/introduction
  */
 
-const charactersCtrl = (req: NextApiRequest, res: NextApiResponse) => {
+const charactersCtrl = async (req: NextApiRequest, res: NextApiResponse) => {
+  const file = await fs.readFile(`${process.cwd()}/mockdata/allCharacters.json`, 'utf-8');
+  const characters = JSON.parse(file);
+
   const body: { data: LickApi.ICharacterCore[] } = {
-    data: []
+    data: characters.data
   };
   res.status(200).json(body);
 };
