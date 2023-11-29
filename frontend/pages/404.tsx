b7e502643f1eed85d8353fe8d8ca1f8417acc4dd
Ufuk Bozdemir <ufb007@gmail.com>
2023-11-29 13:39:43 +0000

    Custom error page created

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,11 @@
+const Custom404 = () => {
+    return (
+      <div className="text-white flex flex-col items-center justify-center font-schwifty h-screen">
+        <img src="/rick-and-morty-logo.png" className="w-1/2 md:w-1/4 mb-10" />
+        <h1 className="text-7xl mb-5">404</h1>
+        <p className="text-4xl">Page Not Found</p>
+      </div>
+    );
+};
+  
+  export default Custom404;
