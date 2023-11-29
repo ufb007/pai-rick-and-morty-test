386c917aea265bc169a5dde1108adb369d95eb2b
Ufuk Bozdemir <ufb007@gmail.com>
2023-11-29 10:22:30 +0000

    Adjusted font size on responsiveness

--- frontend/src/components/_shared/characters/CharacterCard.tsx
+++ frontend/src/components/_shared/characters/CharacterCard.tsx
@@ -8,7 +8,7 @@ const CharacterCard = ({ id, name, gender, species, avatar }: LickApi.ICharacter
     return (
         <CharacterContainer className="w-full sm:w-1/2 md:w-1/4 lg:w-1/5 xl:w-1/6 px-10 md:px-5 text-white flex flex-col mb-10 md:mb-[100px] cursor-pointer">
             <AvatarImg src={avatar} className="w-full" />
-            <ul className="mt-3 flex flex-col gap-2 mb-7 flex-grow">
+            <ul className="mt-3 flex flex-col gap-2 mb-7 flex-grow text-lg md:text-sm">
                 <li>Name: { name }</li>
                 <li>Gender: { gender }</li>
                 <li>Species: { species }</li>
