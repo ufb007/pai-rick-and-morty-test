2f07f025776bf0c19aea49cbab5a6ebf16959070
Ufuk Bozdemir <ufb007@gmail.com>
2023-12-01 10:43:25 +0000

    Looping through pages collecting all characters

--- backend/src/services/CharacterService.ts
+++ backend/src/services/CharacterService.ts
@@ -2,73 +2,106 @@ import axios from "@utils/axios"
 import RickAndMortyAPI from "@interfaces/rick-and-morty-api";
 import CharacterAPI from "@interfaces/character-api";
 import CharacterDTO from "@models/ChracterDTO";
+import { AxiosError, AxiosResponse } from "axios";
 
 // todo possible create character enum for alive | dead | unknown
 
+type CharacterInfoType = {
+    count: number,
+    pages: number,
+    next: string | null;
+}
+
+type RickAndMortyResponseType = {
+    info: CharacterInfoType,
+    results: RickAndMortyAPI.ICharacterCore[]
+}
+
+type ErrorResponseType = {
+    message: string
+}
+
 export default class CharacterService {
-    public static getAllMortys = async (name: string, status: 'alive' | 'dead' | 'unknown'): Promise<CharacterAPI.ICharacter[]> => {
-        // Make request for characters with the name and status defined
-        const characters = await axios(`/character/?name=${name}&status=${status}`);
+    public static getAllMortys = async (name: string, status: 'alive' | 'dead' | 'unknown'): Promise<CharacterAPI.ICharacter[] | ErrorResponseType> => {
+        try {
+            // Make request for characters with the name and status defined
+            const characters: RickAndMortyAPI.ICharacterCore[] = [];
+            let characterInfo: CharacterInfoType = { count: 0, pages: 1, next: '' }
+            let page: number = 0
 
-        const locationIdsForCharacters: {id: number, locationId: number}[] = [];
-        const locationIds: number[] = [];
+            // Loop through however many pages exist
+            while (characterInfo.next !== null) {
+                page++;
 
-        const episodeIdsForCharacters:  {id: number, episodeId: number}[] = [];
-        const episodeIds: number[] = [];
-
-        const aggregatedCharacters: any[] = [];
-
-        // Get characters core data from  
-        characters.data.results.map((character: RickAndMortyAPI.ICharacterCore): void => {
-            const { location, episode } = character;
-
-            const locationId = Number(location.url.substring(location.url.lastIndexOf('/') + 1));
-
-            // Avoiding duplicates
-            if (!locationIds.includes(locationId)) {
-                locationIds.push(locationId);
+                await axios(`/character/?page=${page}&name=${name}&status=${status}`)
+                    .then(({ data: { info, results } }: AxiosResponse<RickAndMortyResponseType>): void => {
+                        characterInfo = info;
+                        characters.push(...results)
+                    })
             }
 
-            locationIdsForCharacters.push({ id: character.id, locationId });
+            const locationIdsForCharacters: {id: number, locationId: number}[] = [];
+            const locationIds: number[] = [];
 
-            episode.map((episode: string) => {
-                const episodeId = Number(episode.substring(episode.lastIndexOf('/') + 1));
+            const episodeIdsForCharacters:  {id: number, episodeId: number}[] = [];
+            const episodeIds: number[] = [];
+
+            const aggregatedCharacters: any[] = [];
+
+            // Get characters core data from  
+            characters.map((character: RickAndMortyAPI.ICharacterCore): void => {
+                const { location, episode } = character;
+
+                const locationId = Number(location.url.substring(location.url.lastIndexOf('/') + 1));
 
                 // Avoiding duplicates
-                if (!episodeIds.includes(episodeId)) {
-                    episodeIds.push(episodeId)
+                if (!locationIds.includes(locationId)) {
+                    locationIds.push(locationId);
                 }
 
-                episodeIdsForCharacters.push({ id: character.id, episodeId });
+                locationIdsForCharacters.push({ id: character.id, locationId });
+
+                episode.map((episode: string) => {
+                    const episodeId = Number(episode.substring(episode.lastIndexOf('/') + 1));
+
+                    // Avoiding duplicates
+                    if (!episodeIds.includes(episodeId)) {
+                        episodeIds.push(episodeId)
+                    }
+
+                    episodeIdsForCharacters.push({ id: character.id, episodeId });
+                });
+
+                aggregatedCharacters.push(CharacterDTO.characterCore(character));
             });
 
-            aggregatedCharacters.push(CharacterDTO.characterCore(character));
-        });
+            // Get multiple locations from one request example: https://rickandmortyapi.com/api/location/3,21
+            const locations = await axios(`/location/${locationIds.join(",")}`);
 
-        // Get multiple locations from one request example: https://rickandmortyapi.com/api/location/3,21
-        const locations = await axios(`/location/${locationIds.join(",")}`);
+            // Get multiple episodes from one request example: https://rickandmortyapi.com/api/episode/3,21
+            const episodes = await axios(`/episode/${episodeIds.join(",")}`);
 
-        // Get multiple episodes from one request example: https://rickandmortyapi.com/api/episode/3,21
-        const episodes = await axios(`/episode/${episodeIds.join(",")}`);
+            // Finally time to combine locations & episodes into one
+            aggregatedCharacters.map((character: CharacterAPI.ICharacterCore & CharacterAPI.ICharacter): void => {
+                const locationId = locationIdsForCharacters.find(({ id }) => id === character.id)?.locationId;
+                const location = locations.data.find(({ id }: RickAndMortyAPI.ILocationDetail) => id === locationId);
 
-        // Finally time to combine locations & episodes into one
-        aggregatedCharacters.map((character: CharacterAPI.ICharacterCore & CharacterAPI.ICharacter): void => {
-            const locationId = locationIdsForCharacters.find(({ id }) => id === character.id)?.locationId;
-            const location = locations.data.find(({ id }: RickAndMortyAPI.ILocationDetail) => id === locationId);
+                character.location = CharacterDTO.location(location);
 
-            character.location = CharacterDTO.location(location);
+                const eps: CharacterAPI.IEpisode[] = []
+                
+                episodeIdsForCharacters.filter(({ id, episodeId }) => {
+                    if (id === character.id) {
+                        eps.push(CharacterDTO.episode(episodes.data.find(({ id }: RickAndMortyAPI.IEpisodeDetail) => id === episodeId)))
+                    }
+                });
 
-            const eps: CharacterAPI.IEpisode[] = []
-            
-            episodeIdsForCharacters.filter(({ id, episodeId }) => {
-                if (id === character.id) {
-                    eps.push(CharacterDTO.episode(episodes.data.find(({ id }: RickAndMortyAPI.IEpisodeDetail) => id === episodeId)))
-                }
+                character.episodes = eps;
             });
 
-            character.episodes = eps;
-        });
-
-        return aggregatedCharacters;
+            return aggregatedCharacters;
+        } catch (error: any) {
+            return { message: error.response.data.error }
+        }
     }
 }
